1d454e9301e744f261b31166b38c1a3176657ddb
NicolasRecabarren <recabarren.valderrama.nicolas@gmail.com>
2020-10-14 00:13:10 -0300

    Estructura del proyecto.

---
+++ src/components/layout/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-navbar variant="dark" type="light" fixed="top">
+        <b-navbar-brand href="#">{{brandText}}</b-navbar-brand>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            brandText: "CHIPAX - TEST"
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    nav.navbar{
+        > a{ color: #FFF; }
+        > a:hover{ color: #41B883; }
+    }
+    
+</style>
