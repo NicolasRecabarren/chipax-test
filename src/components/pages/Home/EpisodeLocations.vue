c1ffaabc7b7b3d62f10074094804cfe9fd0849e1
NicolasRecabarren <recabarren.valderrama.nicolas@gmail.com>
2020-10-16 01:04:16 -0300

    Se agrega tiempo que se demoró en realizar el listado de locations en cada episode.

--- src/components/pages/Home/EpisodeLocations.vue
+++ src/components/pages/Home/EpisodeLocations.vue
@@ -1,11 +1,16 @@
 <template>
-    <b-modal :id="`modal-episode-locations-${episode.id}`" title="Episode Locations Detail">
+    <b-modal :id="`modal-episode-locations-${episode.id}`" title="Episode Locations Detail" hide-footer>
         <h4>EP: {{episode.name}}</h4>
+        Tomó un total de <b>{{endTime}}</b> ms.
+        <p>El Episode tiene un total de <b>{{getLocations.length}}</b> locations (origin).</p>
         <ul>
             <li v-for="(location, index) in getLocations" :key="index">
                 {{location}}
             </li>
         </ul>
+        <b-button block variant="primary" @click="$bvModal.hide(`modal-episode-locations-${episode.id}`)">
+            Cerrar
+        </b-button>
     </b-modal>
 </template>
 
@@ -15,24 +20,38 @@ import { mapGetters } from 'vuex';
 export default {
     props: ['episode'],
 
+    data() {
+        return {
+            startTime: new Date().getTime(),
+            endTime: 0
+        }
+    },
+
     computed: {
         ...mapGetters([
             'rickandmorty/getCharacters'
         ]),
 
         getLocations(){
-            // Obtenemos los personajes que aparecen en el capítulo.
+            // Obtenemos los IDs de los personajes que aparecen en el capítulo.
             const episodeCharacter = this.episode.characters.map( char => parseInt(char.split('/').pop()));
 
             // Vamos a buscar los locations.
-            let results = this['rickandmorty/getCharacters'].results
+            let results = this['rickandmorty/getCharacters']
                 .filter( char => episodeCharacter.includes( char.id ))
                 .map(({origin}) => origin.name);
 
             // Quitamos los elementos duplicados.
             results = [...new Set(results)];
 
+            this.setEndTime();
             return results;
+        }
+    },
+
+    methods: {
+        setEndTime(){
+            this.endTime = new Date().getTime() - this.startTime;
         }
     }
 }
